0ae9d1eb7a33e2ee411fe0081ef39038cd660379
justin97530 <jcraftr1@gmail.com>
2016-01-23 20:35:33 +0530

    Implemented NW.js windows

--- src/client.js
+++ src/client.js
@@ -1,14 +1,21 @@
+var gui = require("nw.gui"),
+	window = gui.Window.get();
+
+window.moveTo(window.screen.availWidth / 2, 0);
+
 $(function() {
-    function summon(options) {
+	function summon(options) {
         var toast = $('<toast></toast>', {class: options.type === 'pill' ? 'pill' : '', 'data-action': options.action ? options.action : ''});
         toast.append($('<img></img>', {class: 'toast-img', src: options.icon ? options.icon : 'default.png'}));
         toast.append($('<div></div>', {class: 'toast-title', text: options.title}));
         toast.append($('<div></div>', {class: 'toast-content', text: options.content}));
         if (options.sound) new Audio('audio.mp3').play();
-        $('.container').append(toast);
+		$(".container").append(toast);
+		window.resizeTo($(document).width(), $(document).height());
+		window.moveTo((window.screen.availWidth - $(document).width()) / 2, 0);
     }
 
-    function pan(options) {
+	function pan(options) {
         if (typeof options === "object" && options.title && options.content) summon(options);
         else throw new Error("Options should be a variable containing title and content.");
     }
